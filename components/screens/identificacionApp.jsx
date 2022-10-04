8eef7a82cb5d6a3b263e696f4e13c29ac164c071
Android Studio <devandroid@ort>
2022-10-04 10:36:51 -0300

    falta scanner y maps

--- components/screens/identificacionApp.jsx
+++ components/screens/identificacionApp.jsx
@@ -1,16 +1,30 @@
 import { StyleSheet, View, Text, Image, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import Scan from '../scan';
 
 export function IdentificacionApp() {
     const navigation = useNavigation();
    /*Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
     startActivity(intent);*/
+   
     return (
         <View>
-            <Text>Identificacion App</Text>
-            <Image source={require('../img/qr.png')}/>
+            <Text style={styles.texto}>Escanea el codigo para averiguar quienes somos!</Text>
+            <Image style={styles.codigo} source={require('../../img/qr.png')}/>
+            <Button title="Presiona aquí para escanear otro codigo"/>
         </View>
           
     );
 }
+
+const styles = StyleSheet.create({
+    codigo: {
+        width:400,
+        height: 400
+    },
+    texto: {
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+});
